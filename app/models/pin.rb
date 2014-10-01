0115e0b9e05c0bd236be4abd92c5c4808c816734
Jarret Belles <jbelles@ucsc.edu>
2014-10-01 15:24:06 -0700

    home page formatting

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,7 +3,7 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image , :styles => { :medium => "300x300>", :thumb => "100x100>" }, whiny: false #:default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-  validates :image, presence: true
+  #validates :image, presence: true
   validates :description, presence: true
 
 end
